d54036539e558732876db212286c65cc5a20d23e
hamza sarwar <hamzasarwarofficial99@gmail.com>
2024-10-29 02:44:47 +0500

    component done

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import First from './src/components/First'
 
 const App = () => {
   return (
     <View>
       <Text style={{ fontSize: 24 }}>Hello am a mobile app developer</Text>
-      <Button title='press me'/>
+      <Button title='press me' />
+      <First />
     </View>
     
   )
---
+++ src/components/First.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const First = () => {
+  return (
+    <View>
+          <Text style = {{fontSize:20}}>This is the first component i have create</Text>
+    </View>
+  )
+}
+
+export default First
+
+const styles = StyleSheet.create({})
