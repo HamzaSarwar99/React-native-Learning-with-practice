bb8549e522aeddbbcfcf545ab474b1bfbc6ff122
hamza sarwar <hamzasarwarofficial99@gmail.com>
2024-10-29 02:55:26 +0500

    Jsx done

--- App.jsx
+++ App.jsx
@@ -1,13 +1,17 @@
 import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import First from './src/components/First'
+import JSX from './src/components/JSX'
 
 const App = () => {
+  const name = 'hamza'
   return (
     <View>
-      <Text style={{ fontSize: 24 }}>Hello am a mobile app developer</Text>
+      {/* <Text style={{ fontSize: 24 }}>Hello am a mobile app developer</Text>
       <Button title='press me' />
-      <First />
+      <First /> */}
+      {/* <Text>My name is {name}</Text> */}
+      <JSX/>
     </View>
     
   )
---
+++ src/components/JSX.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const JSX = () => {
+  return (
+    <View>
+          <Text>WE CAN NOT USE STATEMENTS INSIDE JSX BUT WE CAN 
+              USE EXPRESSIONS.
+              EXPRESSIONS are those  which return some value
+              and the STATEMENTS are those which do not return any value.
+      </Text>
+    </View>
+  )
+}
+
+export default JSX
+
+const styles = StyleSheet.create({})
