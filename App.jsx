68d1aa2c3f992cfbc30ef8c52f26bba88503bb2e
hamza sarwar <hamzasarwarofficial99@gmail.com>
2024-10-29 02:28:53 +0500

    View,Text and Button done

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 24 }}>Hello am a mobile app developer</Text>
+      <Button title='press me'/>
+    </View>
+    
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
